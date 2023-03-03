77c0d21612538802819759d049500dad028ea2fe
stamina777 <lizzy.amina18@yahoo.com>
2023-03-03 10:39:07 -0800

    hello, print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this function serves as the starting point for program execution
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
