2cfb115455ac652aa1d29b9e6a0c15f16b8f7d7a
stamina777 <lizzy.amina18@yahoo.com>
2023-03-04 09:47:49 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 
 	for (i = 0 ; i <= 99 ; i++)
 	{
-		for (j = 0 ; j <= 99 ; j++)
+		for (j = i + 1 ; j <= 99 ; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -23,6 +23,7 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
+			continue;
 			{
 				putchar(',');
 				putchar(' ');
