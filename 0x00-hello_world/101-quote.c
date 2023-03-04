4dac68c689d7a3dda698dcdea83fa374f5c3fc29
stamina777 <lizzy.amina18@yahoo.com>
2023-03-04 02:06:06 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 
 /**
  * main - Prints output
- *
  * Return:1(Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" -
+	Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
