9c61e2980f0267e9e50a74d89d3c9af6be0f5e4c
stamina777 <lizzy.amina18@yahoo.com>
2023-03-05 13:56:21 -0800

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,30 +4,24 @@
  * print_sign - function that prints signs
  *
  *@n: parameter of functions
- * Return: 1, 0 and -1
+ * Return: 1 for positive num, -1 for negative num and zero for anything else
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
+		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		return (0);
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		return (-1);
+		_putchar(48);
+		return (0);
 	}
 }
