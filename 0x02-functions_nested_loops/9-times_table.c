2abe8f4be4d36e5e34d659f87b264db8b8b17f46
stamina777 <lizzy.amina18@yahoo.com>
2023-03-06 00:50:46 -0800

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,11 +14,11 @@ void times_table(void)
 	{
 		for (y = 0 ; y <= 9 ; y++)
 		{
- 		z = x * y;
-		if (z > 9)
+			z = x * y;
+			if (z > 9)
 		{
 			u = z % 10;
-			d = (z - u) / 10;
+			d = ((z - u) / 10);
 			_putchar(44);
 			_putchar(32);
 			_putchar(d + '0');
