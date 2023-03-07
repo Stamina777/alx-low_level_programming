27ae0c22e77525c8c222e9776987bb902644134c
Amina <lizzy.amina18@yahoo.com>
2023-03-07 04:56:18 -0800

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,15 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0 ; x < 14 ; x++)
+	for (x = 1 ; x <= 10 ; x++)
 	{
 	for (y = 0 ; y <= 14 ; y++)
 	{
-	if (y > 0)
+	if (y >= 0)
 	{
-	_putchar((y / 14) + '0');
+	_putchar('1');
 	}
-	_putchar((y % 14) + '0');
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
 	}
